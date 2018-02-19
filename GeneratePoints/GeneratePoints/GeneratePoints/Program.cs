bc8349e653e39e4274be614db92ad386dea1e085
Morten Albring <mortenalbring@gmail.com>
2018-02-19 16:50:11 +0000

    Refactored code to execute povray from program

--- GeneratePoints/GeneratePoints/GeneratePoints/Program.cs
+++ GeneratePoints/GeneratePoints/GeneratePoints/Program.cs
@@ -2,31 +2,30 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+using System.Threading;
 
 namespace GeneratePoints
 {
+    public class Shape
+    {
+        public string ShapeName;
+        public List<AnchorPoint> AnchorPoints = new List<AnchorPoint>();
+    }
     public class AnchorPoint
     {
-        public double x;
-        public double y;
-        public double z;
+        public double X;
+        public double Y;
+        public double Z;
 
-        public double r;
-        public double g;
-        public double b;
+        public double R;
+        public double G;
+        public double B;
     }
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            var shapeName = "octo";
-
+            
             var tetrahedron = GenerateTetrahedron();
 
             var cube = GenerateCube();
@@ -36,29 +35,110 @@ namespace GeneratePoints
             var ico = GenerateIco();
 
 
-            var shape = octo;
-            var anchorPoints = shape.Count;
-            var outputAnchors = shapeName + "-anchors.txt";
+            var shape = octo;            
+            var outputAnchors = shape.ShapeName + "-anchors.txt";
 
 
             var outputAnchorStr = "";            
           
-            for (int i = 0; i < anchorPoints; i++)
+            for (int i = 0; i < shape.AnchorPoints.Count; i++)
             {                
-                var x = shape[i].x;
-                var y = shape[i].y;
-                var z = shape[i].z;
+                var x = shape.AnchorPoints[i].X;
+                var y = shape.AnchorPoints[i].Y;
+                var z = shape.AnchorPoints[i].Z;
                 var outputstr = "<" + x + ", " + y + "," + z + ">";
                 outputAnchorStr = outputAnchorStr + outputstr + ",";
                 
-                outputAnchorStr = outputAnchorStr + "<" + shape[i].r + ", " + shape[i].g + "," + shape[i].b + ">,";
+                outputAnchorStr = outputAnchorStr + "<" + shape.AnchorPoints[i].R + ", " + shape.AnchorPoints[i].G + "," + shape.AnchorPoints[i].B + ">,";
 
             }
 
             File.Delete(outputAnchors);            
             File.AppendAllText(outputAnchors, outputAnchorStr);            
 
-            var maxPoints = 10000000;
+            var maxPoints = 1000000;
+            var ratio = 2;
+            WriteDataPoints(shape, maxPoints,ratio);
+
+            //Console.WriteLine("Done");
+           
+           
+            var frames = 3;
+
+            var inifiles = new List<string>();
+
+            for (int i = 0; i < frames; i++)
+            {
+                PreparePovRayFiles(i,frames);
+
+                var inifile = WritePovrayIniFile(i,frames);
+
+                inifiles.Add(inifile);
+
+              
+            }
+
+            foreach (var file in inifiles)
+            {
+                Console.WriteLine("Rendering " + file);
+             //   Process.Start("C:\\Program Files\\POV-Ray\\v3.7\\bin\\pvengine64.exe", "/RENDER " + file);
+           //     Thread.Sleep(300000);
+            }
+
+        }
+
+        private static void PreparePovRayFiles(int currentFrame,int maxFrames)
+        {
+            var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = System.IO.Path.GetDirectoryName(path);
+
+            var dirsplit = directory.Split('\\');
+            var basedir = dirsplit[0] + "\\" + dirsplit[1];
+            var nocamFile = "fc-nocam.pov";
+
+            var compiledFilename = "fc-nocam_f" + currentFrame + ".pov";
+
+            var nocamPath = Path.Combine(basedir, nocamFile);
+            var compiledFile = Path.Combine(directory, compiledFilename);
+            if (File.Exists(compiledFile))
+            {
+                File.Delete(compiledFile);
+            }
+
+            double clock = ((double) currentFrame / (double) maxFrames);
+
+            File.Copy(nocamPath, compiledFile);
+
+            var cameraString =
+                "\n\n\ncamera {\t\r\n\tlocation <sin(2*pi*" + clock +")*2.5, 0.1, cos(2*pi* + " + clock +")*2.5>\t\t           \r\n\tlook_at <0,0,0>       \t\r\n\trotate <0,0,0>\r\n}  ";
+
+            File.AppendAllText(compiledFile,cameraString);
+        }
+
+        private static string WritePovrayIniFile(int currentFrame, int maxFrames)
+        {
+            var iniFile = "fc-nocam_f" + currentFrame + ".ini";
+
+            var povOutputFilename = "test_" + currentFrame.ToString("00000") + ".png";
+
+            var lines = new List<string>();
+            lines.Add("Input_File_Name=fc-nocam_f" + currentFrame + ".pov\n");
+            lines.Add("Output_File_Name=" + povOutputFilename + "\r\n");                     
+            File.WriteAllLines(iniFile,lines);
+
+            var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = System.IO.Path.GetDirectoryName(path);
+
+                  
+            var inifilepath = Path.Combine(directory, iniFile);
+
+           
+            return inifilepath;
+
+        }
+
+        private static void WriteDataPoints(Shape shape, int maxPoints, int ratio)
+        {
             var rnd = new Random();
             var output = "";
 
@@ -70,7 +150,8 @@ namespace GeneratePoints
             var gPoint = 0.0;
             var bPoint = 0.0;
 
-            var outputfilename = shapeName + maxPoints + "-datapoints.txt";
+          
+            var outputfilename = shape.ShapeName + "_r" + ratio + "_p" + maxPoints + "-datapoints.txt";
             File.Delete(outputfilename);
             var sw = new Stopwatch();
             sw.Start();
@@ -78,16 +159,15 @@ namespace GeneratePoints
 
             for (int i = 0; i < maxPoints; i++)
             {
-                var val = rnd.Next(0, anchorPoints);
+                var val = rnd.Next(0, shape.AnchorPoints.Count);
 
-                xPoint = (xPoint + shape[val].x) / 2;
-                yPoint = (yPoint + shape[val].y) / 2;
-                zPoint = (zPoint + shape[val].z) / 2;
+                xPoint = (xPoint + shape.AnchorPoints[val].X) / ratio;
+                yPoint = (yPoint + shape.AnchorPoints[val].Y) / ratio;
+                zPoint = (zPoint + shape.AnchorPoints[val].Z) / ratio;
 
-                rPoint = (rPoint + shape[val].r) / 2;
-                gPoint = (gPoint + shape[val].g) / 2;
-                bPoint = (bPoint + shape[val].b) / 2;
-
+                rPoint = (rPoint + shape.AnchorPoints[val].R) / ratio;
+                gPoint = (gPoint + shape.AnchorPoints[val].G) / ratio;
+                bPoint = (bPoint + shape.AnchorPoints[val].B) / ratio;
 
 
                 var outputstr = "<" + xPoint + ", " + yPoint + "," + zPoint + ">";
@@ -95,7 +175,6 @@ namespace GeneratePoints
 
                 output = output + "<" + rPoint + ", " + gPoint + "," + bPoint + ">,";
 
-               
 
                 cWriteCount++;
                 if (cWriteCount == 1000)
@@ -104,7 +183,7 @@ namespace GeneratePoints
                     output = "";
                     cWriteCount = 0;
 
-                    double timePerElem = (double)sw.Elapsed.TotalSeconds / (double)(i + 1);
+                    double timePerElem = (double) sw.Elapsed.TotalSeconds / (double) (i + 1);
                     var elemsRemaining = (maxPoints - i);
                     var minsRemaining = ((elemsRemaining * timePerElem) / 60).ToString("N");
 
@@ -113,9 +192,6 @@ namespace GeneratePoints
             }
 
             File.AppendAllText(outputfilename, output);
-
-            Console.ReadLine();
-
         }
 
         private static List<AnchorPoint> GenerateTetrahedron()
@@ -172,23 +248,26 @@ namespace GeneratePoints
                 
                 var anch = new AnchorPoint();
 
-                anch.x = anchor[0];
-                anch.y = anchor[1];
-                anch.z = anchor[2];
+                anch.X = anchor[0];
+                anch.Y = anchor[1];
+                anch.Z = anchor[2];
 
                 var color = colours[p];
 
-                anch.r = color[0];
-                anch.g = color[1];
-                anch.b = color[2];
+                anch.R = color[0];
+                anch.G = color[1];
+                anch.B = color[2];
                 output.Add(anch);
                 p++;
             }
             return output;
         }
 
-        private static List<AnchorPoint> GenerateCube()
+        private static Shape GenerateCube()
         {
+            var anch = new Shape();
+            anch.ShapeName = "cube";
+
             var anchors = new List<List<double>>();
             var anchor1 = new List<double> { -1, -1, -1 };
             var anchor2 = new List<double> { 1, -1, -1 };
@@ -205,11 +284,16 @@ namespace GeneratePoints
             anchors.Add(anchor6);
 
             var output = MakeAnchorPoints(anchors);
-            return output;
+            anch.AnchorPoints = output;
+
+            return anch;
         }
 
-        private static List<AnchorPoint> GenerateOctahedron()
+        private static Shape GenerateOctahedron()
         {
+            var anch = new Shape();
+            anch.ShapeName = "octahedron";
+
             var anchors = new List<List<double>>();
             var anchor1 = new List<double> { -1, 0, 0 };
             var anchor2 = new List<double> {  1, 0, 0 };
@@ -226,10 +310,15 @@ namespace GeneratePoints
             anchors.Add(anchor6);
 
             var output = MakeAnchorPoints(anchors);
-            return output;
+            anch.AnchorPoints = output;
+
+            return anch;
         }
-        private static List<AnchorPoint> GenerateIco()
+        private static Shape GenerateIco()
         {
+            var anch = new Shape();
+            anch.ShapeName = "ico";
+
             var anchors = new List<List<double>>();
             var phi = (1 + Math.Sqrt(5)) / 2;
 
@@ -258,7 +347,9 @@ namespace GeneratePoints
             anchors.Add(anchor10);
 
             var output = MakeAnchorPoints(anchors);
-            return output;
+            anch.AnchorPoints = output;
+
+            return anch;
         }
 
     }
